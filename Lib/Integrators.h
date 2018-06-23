eb1e06c89022037ae28f71ad7d06b908b479f3a3
Kristjan Kongas <kongaskristjan@gmail.com>
2018-06-23 22:39:30 +0300

    Add Runge Kutta 4 integrator

--- Lib/Integrators.h
+++ Lib/Integrators.h
@@ -17,12 +17,26 @@
 */
 
 template<typename IntegrableState, typename Differetiator>
-void advanceEuler(IntegrableState &state, const Differetiator &differetiator, double step);
+void advanceEuler(IntegrableState &x, const Differetiator &diff, double dT);
+
+template<typename IntegrableState, typename Differetiator>
+void advanceRungeKutta4(IntegrableState &x, const Differetiator &diff, double dT);
 
 
 template<typename IntegrableState, typename Differetiator>
-void advanceEuler(IntegrableState &x, const Differetiator &differetiator, double dT) {
-    x = x + differetiator.derivative(x) * dT;
+void advanceEuler(IntegrableState &x, const Differetiator &diff, double dT) {
+    x = x + diff.derivative(x) * dT;
+}
+
+template<typename IntegrableState, typename Differetiator>
+void advanceRungeKutta4(IntegrableState &x, const Differetiator &diff, double dT) {
+    // Equations from https://en.wikipedia.org/wiki/Runge-Kutta_methods
+
+    IntegrableState k1 = diff.derivative(x) * dT;
+    IntegrableState k2 = diff.derivative(x + k1 * 0.5) * dT;
+    IntegrableState k3 = diff.derivative(x + k2 * 0.5) * dT;
+    IntegrableState k4 = diff.derivative(x + k3) * dT;
+    x = x + (k1 + k2 * 2 + k3 * 2 + k4) * (1. / 6.);
 }
 
 #endif
--- Tests/IntegratorTest.cpp
+++ Tests/IntegratorTest.cpp
@@ -2,6 +2,7 @@
 #include "Lib/Integrators.h"
 #include <gtest/gtest.h>
 #include <cmath>
+#include <iostream>
 
 // Integrate x(0) = 1, dx/dt = x. x(1) should be e
 
@@ -12,15 +13,17 @@ public:
     }
 };
 
-TEST(IntegrableTest, Euler) {
-    double x = 1;
+TEST(IntegrableTest, Exponent) {
+    double xEuler = 1, xRK4 = 1;
     int nSteps = 1000;
     double dT = 1. / nSteps;
 
     Linear diff;
     for(int i = 0; i < nSteps; ++i) {
-        advanceEuler(x, diff, dT);
+        advanceEuler(xEuler, diff, dT);
+        advanceRungeKutta4(xRK4, diff, dT);
     }
 
-    EXPECT_NEAR(x, M_E, 1e-2);
+    EXPECT_NEAR(xEuler, M_E, 1e-2);
+    EXPECT_NEAR(xRK4, M_E, 1e-8);
 }
