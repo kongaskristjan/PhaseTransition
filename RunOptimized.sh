507bea8210c249f1c2f75c5ca2a353c4a2251345
Kristjan Kongas <kongaskristjan@gmail.com>
2018-07-11 22:42:25 +0300

    Equalize build flags in tests and running mode to improve build time

--- RunOptimized.sh
+++ RunOptimized.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bazel test --compilation_mode=dbg //... && bazel build --compilation_mode=opt //... && bazel-bin/PhaseTransition/PhaseTransition
+bazel test --compilation_mode=opt //... && bazel build --compilation_mode=opt //... && bazel-bin/PhaseTransition/PhaseTransition
