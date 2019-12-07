94f31135a676ee3e6e3595c8a7da23de276ddb81
Kristjan Kongas <kongaskristjan@gmail.com>
2019-12-07 17:48:52 +0200

    Improve performance by adding spacers to atomic counter variable

--- Lib/AtomicCounter.h
+++ Lib/AtomicCounter.h
@@ -14,7 +14,11 @@ public:
     std::size_t total() const;
 
 private:
+    // spacer1 and spacer2 force non-atomic variables to different cache lines than atomic variables
+    // This significantly improves performance of using address-wise nearby variables
+    char spacer1[256];
     std::atomic<std::size_t> idx;
+    char spacer2[256];
     std::size_t total_;
 };
 
